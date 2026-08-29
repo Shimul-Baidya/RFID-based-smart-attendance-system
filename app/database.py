1b69310fe13c9e10ce2664cbb0433751940d4fae
Shimul-Baidya <shimul.cse31@gmail.com>
2026-08-29 13:32:21 +0600

    feat: add User SQLAlchemy model and Pydantic schemas

--- app/database.py
+++ app/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.config import database_url
 
-
 engine = create_engine(database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
